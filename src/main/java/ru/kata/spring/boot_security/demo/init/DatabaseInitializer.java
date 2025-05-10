7ef80d6e04b09143494b746a44d662cea3f0cac7
Avalanchez <login.inside@gmail.com>
2025-05-10 14:10:51 +0300

    Create default users

---
+++ src/main/java/ru/kata/spring/boot_security/demo/init/DatabaseInitializer.java
@@ -0,0 +1,58 @@
+package ru.kata.spring.boot_security.demo.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.Set;
+
+@Component
+public class DatabaseInitializer implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public DatabaseInitializer(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) {
+        if (roleRepository.count() == 0) {
+            Role adminRole = new Role("ROLE_ADMIN");
+            Role userRole = new Role("ROLE_USER");
+            roleRepository.save(adminRole);
+            roleRepository.save(userRole);
+
+            User admin = new User();
+            admin.setUsername("admin");
+            admin.setPassword(passwordEncoder.encode("admin")); // Для теста без шифрования
+            admin.setName("Admin");
+            admin.setSurname("Adminov");
+            admin.setDepartment("IT");
+            admin.setSalary(100000);
+            admin.setRoles(Set.of(adminRole, userRole));
+            userRepository.save(admin);
+
+            User user = new User();
+            user.setUsername("user");
+            user.setPassword(passwordEncoder.encode("user"));
+            user.setName("User");
+            user.setSurname("Userov");
+            user.setDepartment("HR");
+            user.setSalary(50000);
+            user.setRoles(Set.of(userRole));
+            userRepository.save(user);
+        }
+    }
+}
